e2ce8436853d41ce72ee1bef254e52166d0a94e3
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-09-30 01:02:10 +0530

    Time: 7 ms (57.01%), Space: 17.2 MB (5.74%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+     map<int,char> mp;
+      int z=0;
+     string r="";
+
+      for(int i=0;i<indices.size();i++)
+          mp[indices[i]]=s[i];
+
+      for(auto x:mp)
+      {
+          if(z==x.first)
+        {
+            r+=x.second;
+            z++;
+        }
+          else 
+          continue;
+      }  
+      return r;
+    }
+};
