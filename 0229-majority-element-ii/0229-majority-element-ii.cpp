da1a1f92e855bdc2fe538c7fda59056b03db6327
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-10-05 21:43:15 +0530

    Time: 16 ms (7.44%), Space: 16.4 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -6,13 +6,13 @@ public:
         vector<int> res;
         for(int i=0;i<nums.size();i++)
             mp[nums[i]]++;
-        
+        nums.clear();
         for(auto x:mp){
             if(x.second>len)
-                res.push_back(x.first);
+                nums.push_back(x.first);
             
         }
-        return res;
+        return nums;
             
     }
 };
