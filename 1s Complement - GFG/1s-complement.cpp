314d74bb8de3e16e1528196e245304560806dd13
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-10-10 19:22:57 +0530

    Added solution - LeetHub

---
+++ 1s Complement - GFG/1s-complement.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+ 
+#include<bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+  
+class Solution{ 
+public:
+    string onesComplement(string S,int N){
+      string C="";
+      for(int i=0;i<S.length();i++)
+      {
+          if(S[i]=='1')
+          C+='0';
+          else
+          C+='1';
+                }
+                return C;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.onesComplement(s,n)<<"\n";
+    }
+}
+// } Driver Code Ends
