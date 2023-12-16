a9f95ee5e86e4940c8b1d524cded25ff61b94afe
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-12-16 15:17:12 +0530

    Time: 8 ms (62.96%), Space: 7.8 MB (45.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    //    sort(s.begin(),s.end());
-    //    sort(t.begin(),t.end());
-    //    bool r;
-    //    (s==t)? r= true: r= false;
-    //    return r;
-
-       //can be solved using hash table
          if(s.length()!=t.length())
-       return false;
-
-      bool r=false;
-       int count=0;
+          return false;
 
        sort(s.begin(),s.end());
        sort(t.begin(),t.end());
 
        for(int i=0;i<s.length();i++){
-           if(s[i]==t[i]){
-               count+=1;
-           }
-           else{
-               count+=0;
-           }
+           if(s[i]!=t[i])
+           return false;
        }
-           if(count==s.length())
-          r=true;
-          else
-          r=false;
-     return r;
+           
+     return true;
     }
 };
