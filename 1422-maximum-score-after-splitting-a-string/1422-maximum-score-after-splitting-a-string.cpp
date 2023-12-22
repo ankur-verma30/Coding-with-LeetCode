ddc01620944db6b7a0573002476debd846d39af9
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-12-22 14:35:10 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (87.50%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int TotalOnes=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1')
+            TotalOnes++;
+        }
+
+        int mx=0,ZeroCount=0,OneCount=0;
+
+        for(int i=0;i<s.length()-1;i++){
+            if(s[i]=='0')
+            ZeroCount++;
+            else
+            OneCount++;
+            int Remaining=TotalOnes-OneCount;
+            int sum=ZeroCount+Remaining;
+            mx=max(sum,mx);
+
+        }
+        return mx;
+    }
+};
