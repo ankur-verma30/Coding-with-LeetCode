6f2bd2f45769c659b722c08d335586090728654f
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-12-24 13:20:47 +0530

    Time: 6 ms (35.61%), Space: 7.4 MB (24.33%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minOperations(string s) {
+        /*
      int count=0;
 
      for(int i=0;i<s.length();i++){
@@ -9,5 +10,21 @@ public:
      }
 
      return min(count,(int)s.length()-count);
+     */
+     int count=0;
+        
+        for(int i=1;i<s.length();i++){
+            if(s[i-1]==s[i]){
+                if(s[i-1]=='0'){
+                    s[i]='1';
+                    count++;
+                }
+                else if(s[i-1]=='1'){
+                    s[i]='0';
+                    count++;
+                }
+            }
+        }
+        return min(count,(int)s.length()-count);
     }
 };
