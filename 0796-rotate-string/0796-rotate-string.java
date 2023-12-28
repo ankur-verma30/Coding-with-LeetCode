ac8f3a26e3a50922da136c8e67d206ff1d1e1a43
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-12-29 00:39:49 +0530

    Time: 1 ms (34.22%), Space: 41.2 MB (18.46%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,22 +1,10 @@
-/**
- * @param {string} s
- * @param {string} goal
- * @return {boolean}
- */
-var rotateString = function(s, goal) {
-        let st=s+s;
-        let len=goal.length;
+class Solution {
+    public boolean rotateString(String s, String goal) {
 
-        let value=false;
-
-        for(let i=0;i<st.length;i++){
-           let str=st.substring(i,i+len);
-           if(str==goal && (str.length===(st.length/2))) {     
-                value=true;
-                break; 
-        }
-        else 
-        value=false;
-        }
-        return value;
+        String str=s+s;
+    return (str.contains(goal) && goal.length()==(int)str.length()/2)?true:false;
+    
+       
     }
+    
+}
