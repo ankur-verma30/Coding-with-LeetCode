706ca1cc7633b45ad529e310fe9379423a05b137
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2023-12-29 09:40:57 +0530

    Time: 100 ms (5.01%), Space: 43.1 MB (75.67%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -1,12 +1,14 @@
-class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        
-        leng = len(letters)
-        for i in range(0, leng):
-            if letters[i] > target:
-                return letters[i]
-        return letters[0]
-    
-       
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+     for (let i = 0; i < letters.length; i++) {
+    if (letters[i].charCodeAt(0) - target.charCodeAt(0) > 0) {
+        return letters[i];
+    }
+}
+return letters[0];
 
-
+};
