b7d4e1e62a06ce81bedeafb23172011243f14779
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-01-01 05:57:25 +0530

    Time: 36 ms (10.24%), Space: 17.9 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,23 +3,22 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        
-        auto it1=g.begin();
-        auto it2=s.begin();   
-          
-          int count=0;
 
-        while(it1!=g.end() && it2!=s.end()){
-            if(*it1<=*it2)
-            {
+        int count=0,i=0,j=0;
+
+        while(i<(int)g.size() && j<(int)s.size()){
+            if(s[j]>=g[i]){
+                i++;
+                j++;
                 count++;
-                it1++;
-                it2++;
             }
-            else if(*it1>*it2){
-                it2++;
+            else if(s[j]<g[i]){
+                j++;
             }
         }
-      return count;
+
+        return count;
+
+        
     }
 };
