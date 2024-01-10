8e7b2cfe8b201606baf28fa75eab01dfbd4f7644
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-01-10 22:41:04 +0530

    Time: 24 ms (92.41%), Space: 24.6 MB (13.00%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int,string,greater<int>>mp;
+        
+        for(int i=0;i<names.size();i++){
+            mp[heights[i]]=names[i];
+        }
+
+        vector<string>name;
+        for(auto x:mp) name.push_back(x.second);
+
+        return name;
+        
+    }
+};
