774d8cf1a53f45e94c5f1586452de6317cf341f4
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-02-05 02:33:03 +0530

    Time: 0 ms (100.00%), Space: 22.1 MB (40.00%) - LeetHub

---
+++ 3024-type-of-triangle-ii/3024-type-of-triangle-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        if(nums[0]+nums[1]>nums[2]){
+             if((nums[0]==nums[1] || nums[1]==nums[2] )&& nums[0]!=nums[2]) return "isosceles";
+           else if(nums[0]==nums[1] && nums[1]==nums[2] && nums[0]==nums[2]) return "equilateral"; 
+            else return "scalene";
+        }
+        else return "none";
+    }
+};
