d74fb0c6b14659965104420012b678c5ddbefd18
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-02-19 08:19:07 +0530

    Time: 5 ms (20.09%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-       if(n==0)
-    return false;
-    if((n==1)||((n%2==0)&&isPowerOfTwo(n/2)))
-    return true;
-    return 0;
-}
-
+        if(n<=0) return false;
+        
+        return((n&(n-1))==0);
+        
+    
+    }
 };
