9ba9019b3142aa5217fbc924770ce912aeb0b751
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-04-16 15:37:08 +0530

    Time: 32 ms (70.18%), Space: 18.5 MB (21.57%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        
+        int n=s.length();
+
+        for(int i=0;i<n/2;i++){
+            char ch=min(s[i],s[n-i-1]);
+            s[i]=ch;
+            s[n-i-1]=ch;
+        }
+
+        return s;
+    }
+};
