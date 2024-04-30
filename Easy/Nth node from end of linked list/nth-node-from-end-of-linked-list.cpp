e6bb08dce6a1077bc5e0819c54ba06b0b353d58d
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-04-30 18:53:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Nth node from end of linked list/nth-node-from-end-of-linked-list.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial Template for C++
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+class Solution{
+public:
+    int getNthFromLast(Node *head, int n)
+    {
+        if(head==nullptr) return -1;
+          int len=0;
+          Node *temp=head;
+          
+          while(temp){
+              len++;
+              temp=temp->next;
+          }
+          if(n>len) return -1;
+          
+          int currlen=len-n+1;
+          int val=0;
+          
+          Node* temp2=head;
+          while(currlen--){
+              val=temp2->data;
+              temp2=temp2->next;
+          }
+          
+          return val;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+    cout<<obj.getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
