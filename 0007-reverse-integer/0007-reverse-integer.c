8a558b0c0a1192bee18efefbca65e0be2800d295
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-07-04 10:22:06 +0530

    Time: 0 ms (100%), Space: 5.3 MB (73.42%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,13 @@
+int reverse(int y){
+    long new_num=0;
+        int rem=0;
+        while(y!=0){  
+            rem=y%10;
+            new_num=new_num*10+rem;
+             if(new_num>INT_MAX || new_num<INT_MIN) return 0;
+            y/=10;
+        }
+
+       
+        return new_num; 
+}
