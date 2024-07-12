f19f9990197722b3d7857ad05044b9e2539eae50
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-07-12 14:41:10 +0530

    Time: 137 ms (13.82%), Space: 23 MB (50.81%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int points(string& s, string temp, int x) {
+        stack<char> st;
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (st.size() == 0)
+                st.push(s[i]);
+            else {
+                if (s[i] == temp[1] && st.top() == temp[0]) {
+                    total += x;
+                    st.pop();
+                } else
+                    st.push(s[i]);
+            }
+        }
+
+        s = "";
+        while (!st.empty()) {
+            s += st.top();
+            st.pop();
+        }
+        reverse(s.begin(), s.end());
+        return total;
+    }
+
+    int maximumGain(string s, int x, int y) {
+        string s1 = "ab", s2 = "ba";
+        if (x < y)
+            swap(s1, s2), swap(x, y);
+        return points(s, s1, x) + points(s, s2, y);
+    }
+};
