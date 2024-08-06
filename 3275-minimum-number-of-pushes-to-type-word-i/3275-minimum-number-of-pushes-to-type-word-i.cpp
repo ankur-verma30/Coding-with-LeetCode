378526966ef95b1f20893091272a440d26e2aa34
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-08-06 21:11:44 +0530

    Time: 4 ms (33.96%), Space: 11.2 MB (5.45%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-static bool compare(const pair<char,int>pair1,const pair<char,int>pair2){
-    return pair1.second>pair2.second;
- }
     int minimumPushes(string word) {
-       unordered_map<char,int>wordCount;
-       vector<pair<char,int>>words;
+        unordered_map<char, int> wordCount;
+        vector<int> words;
 
-       for(auto& x:word) wordCount[x]++;
+        for (auto& x : word)
+            wordCount[x]++;
 
-       for(auto& x:wordCount){
-        pair<char,int>temp={x.first,x.second};
-        words.push_back(temp);
-       }
+        for (auto& x : wordCount)
+            words.push_back(x.second);
 
-       sort(words.begin(),words.end(),compare);
+        sort(words.rbegin(), words.rend());
 
-       int i=1,count=1,minpush=0;
+        int i = 1, count = 1, minpush = 0;
 
-
-       for(auto x:words){
-        if(count>8){
-            count=1;
-            i++;
+        for (auto x : words) {
+            if (count > 8) {
+                count = 1;
+                i++;
+            }
+            minpush += (x * i);
+            count++;
         }
-        minpush+=(x.second*i);
-        cout<<minpush<<endl;
-        count++;
-        
-       }
-       return minpush; 
+        return minpush;
     }
 };
