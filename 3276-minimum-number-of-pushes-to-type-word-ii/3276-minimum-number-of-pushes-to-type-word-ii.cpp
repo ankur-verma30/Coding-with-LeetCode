b9c3a24ab5f7f811d254a927beba2c49e902cf5d
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-08-06 16:46:52 +0530

    Time: 103 ms (32.19%), Space: 27.7 MB (14.43%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+ static bool compare(const pair<char,int>pair1,const pair<char,int>pair2){
+    return pair1.second>pair2.second;
+ }
+    int minimumPushes(string word) {
+
+       unordered_map<char,int>wordCount;
+       vector<pair<char,int>>words;
+
+       for(auto& x:word) wordCount[x]++;
+
+       for(auto& x:wordCount){
+        pair<char,int>temp={x.first,x.second};
+        words.push_back(temp);
+       }
+
+       sort(words.begin(),words.end(),compare);
+
+       int i=1,count=1,minpush=0;
+
+
+       for(auto x:words){
+        if(count>8){
+            count=1;
+            i++;
+        }
+        minpush+=(x.second*i);
+        cout<<minpush<<endl;
+        count++;
+        
+       }
+       return minpush;
+
+    }
+};
