8bd6f853c82e4c9096597a6289b7d4d63181ec24
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-09-03 13:06:40 +0530

    Time: 84 ms (22.49%), Space: 216.4 MB (5.37%) - LeetHub

---
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string InvertString(string temp) {
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp[i] == '0')
+                temp[i] = '1';
+            else
+                temp[i] = '0';
+        }
+
+        return temp;
+    }
+    string Concat(string str) {
+        string newStr = str;
+        newStr += '1';
+        string temp = InvertString(str);
+         reverse(temp.begin(), temp.end());
+        newStr += temp;
+
+        return newStr;
+    }
+
+    char findKthBit(int n, int k) {
+        string str = "0";
+        for (int i = 1; i <= n; i++) {
+            string temp = Concat(str);
+            str = temp;
+        }
+
+        char ans;
+        for (int i = 0; i < k; i++)
+            ans = str[i];
+
+        return ans;
+    }
+};
