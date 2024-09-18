942f535a25b9d871d419446d2feec6f9def064e1
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-09-18 13:21:07 +0530

    Time: 5 ms (70.7%), Space: 17.4 MB (14.46%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    bool static compare(const string &a, const string &b){ return a+b>b+a;}
+    bool static compare(const string& a, const string& b) {
+        return a + b > b + a;
+    }
     string largestNumber(vector<int>& nums) {
-        int val=count(nums.begin(),nums.end(),0);
-        if(val==nums.size()) return "0";
+
         string res;
-        vector<string>temp;
+        vector<string> temp;
 
-        for(int i=0;i<nums.size();i++) temp.push_back(to_string(nums[i]));
+        for (int i = 0; i < nums.size(); i++)
+            temp.push_back(to_string(nums[i]));
 
-        sort(temp.begin(),temp.end(),compare);
+        sort(temp.begin(), temp.end(), compare);
+        if (temp[0] == "0")
+            return "0";
 
-
-        for(auto x:temp) res+=x;
+        for (auto x : temp)
+            res += x;
 
         return res;
     }
