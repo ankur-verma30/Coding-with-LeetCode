ec64fa6a2dbb3445dd8dd460aab4de1a34fb6896
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-10-04 14:44:46 +0530

    Time: 68 ms (36.8%), Space: 63.1 MB (5%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skills) {
-      long long sum=0;
-      int n=skills.size();
+        long long sum = 0;
+        int n = skills.size();
 
-      for(auto &x:skills) sum+=x;
+        for (auto& x : skills)
+            sum += x;
+        n >>= 1;
+        if (sum % n != 0)
+            return -1;
+        long long target = sum / n;
 
-      long long target= (2*sum)/n;
+        unordered_map<int, int> mpp;
+        vector<pair<int, int>> res;
 
-      unordered_map<int,int>mpp;
-    vector<pair<int,int>>res;
-
-      for(auto &x:skills){
-        if(mpp.find(target-x)!=mpp.end() && target){
-            res.push_back({target-x,x});
-            mpp[target-x]--;
-            if(mpp[target-x]==0) mpp.erase(target-x);
+        for (auto& x : skills) {
+            if (mpp.find(target - x) != mpp.end() && target) {
+                res.push_back({target - x, x});
+                mpp[target - x]--;
+                if (mpp[target - x] == 0)
+                    mpp.erase(target - x);
+            } else
+                mpp[x]++;
         }
-        else mpp[x]++;
-      }
-      if(res.size()!=n/2) return -1;
-    long long pro=0;
-      for(auto &x:res){
-        pro+=(x.first*x.second);
-      }
+        if (res.size() != n)
+            return -1;
+        long long pro = 0;
+        for (auto& x : res) {
+            pro += (x.first * x.second);
+        }
 
-      return pro;
-
+        return pro;
     }
 };
