3ba3736180e486f5d6d36dd451d6f41400752213
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-10-08 23:13:07 +0530

    Time: 101 ms (70.15%), Space: 32.6 MB (91.34%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        // using stack method
+        /* using stack method
         stack<char> st;
         int n = s.length();
         if(n==2) return 0;
@@ -9,12 +9,27 @@ public:
         for (int i = 0; i < n ; i++) {
             if ( !st.empty() && st.top()== '[' && s[i]==']' )
                 st.pop();
-             else 
+             else
                 st.push(s[i]);
-              
+
         }
 
         int unbalanced = st.size()/2;
         return (unbalanced+1) / 2;
+        */
+
+        // Using two pointer
+        int maxCount = 0, tempCount = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == ']') {
+                tempCount++;
+                maxCount = max(maxCount, tempCount);
+            } else
+                tempCount--;
+        }
+
+        return (maxCount + 1) / 2;
     }
 };
