c1c1922661c54ba9639e4646806848e3c9bede75
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-10-21 23:56:17 +0530

    Time: 275 ms (39.18%), Space: 64.4 MB (43.27%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    void Backtrack(int index, string& s, set<string>& st, int& maxCount) {
-        int n = s.length();
+    void Backtrack(int index,int n, string& s, set<string>& st, int& maxCount) {
         if (index == n) {
             maxCount = max(maxCount, (int)st.size());
             return;
@@ -11,16 +10,17 @@ public:
             string str = s.substr(index, i - index + 1);
             if (st.count(str) == 0) {
                 st.insert(str);
-                Backtrack(i + 1, s, st, maxCount);
+                Backtrack(i + 1,n, s, st, maxCount);
                 st.erase(str);
             }
         }
     }
     int maxUniqueSplit(string s) {
         int maxCount = INT_MIN;
+        int n=s.length();
         set<string> st;
 
-        Backtrack(0, s, st, maxCount);
+        Backtrack(0,n, s, st, maxCount);
         return maxCount;
     }
 };
