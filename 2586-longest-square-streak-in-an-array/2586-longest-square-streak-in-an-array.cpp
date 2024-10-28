fdf779bac8365ef857d6e3fca184a83abcae69b5
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-10-29 00:54:03 +0530

    Time: 95 ms (57.78%), Space: 108.8 MB (75.24%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    bool isSqrtN(int n){
-        int sqrtn=sqrt(n);
-        return sqrtn*sqrtn==n;
+    bool isSqrtN(int n) {
+        int sqrtn = sqrt(n);
+        return sqrtn * sqrtn == n;
     }
     int longestSquareStreak(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        sort(nums.begin(),nums.end());
+        unordered_map<int, int> mpp;
+        sort(nums.begin(), nums.end());
 
-      
-        for(int i=0;i<nums.size();i++){
-            if(isSqrtN(nums[i])){
-                int val=sqrt(nums[i]);
-                if(mpp.find(val)!=mpp.end()){
-                    int count=mpp[val];
-                    mpp[nums[i]]=count+1;
-                }
-                else mpp[nums[i]]=1;
-            }
-            else mpp[nums[i]]=1;
+        int maxCount = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (isSqrtN(nums[i])) {
+                int val = sqrt(nums[i]);
+                if (mpp.find(val) != mpp.end()) {
+                    int count = mpp[val];
+                    maxCount = max(maxCount, count + 1);
+                    mpp[nums[i]] = count + 1;
+                } else
+                    mpp[nums[i]] = 1;
+            } else
+                mpp[nums[i]] = 1;
         }
 
-        int maxCount=-1;
-        for(auto x:mpp) maxCount=max(maxCount,x.second);
-
-        return maxCount==1?-1:maxCount;
-
+        return maxCount;
     }
 };
