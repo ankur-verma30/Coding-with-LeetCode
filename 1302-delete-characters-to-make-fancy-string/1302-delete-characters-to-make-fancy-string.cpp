a6a534d306bead181e0e17e61650bbb489b5ce90
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-11-01 11:46:40 +0530

    Time: 35 ms (31.17%), Space: 42.9 MB (30.23%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     string makeFancyString(string s) {
+
+        if(s.length()<3) return s;
         string str = "";
         int count = 1;
+        
 
         char prev = s[0];
-        str+=prev;
+        str += prev;
 
         for (int i = 1; i < s.length(); i++) {
             if (s[i] == prev) {
-                    count++;
-                if (count <= 2)  str += s[i];
-                 else continue;
+                count++;
+                if (count <= 2)
+                    str += s[i];
+                else
+                    continue;
             } else {
                 str += s[i];
                 prev = s[i];
