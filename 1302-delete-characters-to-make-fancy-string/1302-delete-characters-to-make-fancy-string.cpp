c302b3f1a1fcb2fda94dc7e2813d5e612971eed6
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-11-01 11:44:05 +0530

    Time: 33 ms (35.07%), Space: 42.6 MB (77.67%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string str = "";
+        int count = 1;
+
+        char prev = s[0];
+        str+=prev;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == prev) {
+                    count++;
+                if (count <= 2)  str += s[i];
+                 else continue;
+            } else {
+                str += s[i];
+                prev = s[i];
+                count = 1;
+            }
+        }
+        return str;
+    }
+};
