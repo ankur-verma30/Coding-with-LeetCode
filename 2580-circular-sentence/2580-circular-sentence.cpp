c15536ff10d46c95b584fc358c60d5f107bee44a
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-11-02 11:22:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (31.3%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     bool isCircularSentence(string sen) {
-        if(sen[0]!=sen[sen.length()-1]) return false;
-        stringstream ss(sen);
-    string Word;
+        int n=sen.length();
+        if(sen[0]!=sen[n-1]) return false;
+    //     stringstream ss(sen);
+    // string Word;
 
-    vector<char>temp;
-    while (ss >> Word){
-      temp.push_back(Word[0]);
-      temp.push_back(Word[Word.length()-1]);
-    }
+    // vector<char>temp;
+    // while (ss >> Word){
+    //   temp.push_back(Word[0]);
+    //   temp.push_back(Word[Word.length()-1]);
+    // }
     
-    int n=temp.size();
-    for(int i=1;i<n-1;i+=2){
-       if(temp[i]!=temp[i+1]) return false;
-    }
+    // int n=temp.size();
+    // for(int i=1;i<n-1;i+=2){
+    //    if(temp[i]!=temp[i+1]) return false;
+    // }
 
+    for(int i=1;i<n-1;i++){
+        if(sen[i]==' '){
+            if(sen[i-1]!=sen[i+1]) return false;
+        }
+    }
     return true;
     }
 };
