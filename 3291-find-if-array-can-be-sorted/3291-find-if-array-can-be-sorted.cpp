b56131e7aa2adbc8171c036f06a390cfc3e7eac7
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-11-06 10:35:16 +0530

    Time: 12 ms (15.11%), Space: 31.1 MB (86.44%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        int n=nums.size();
-       
+        int n = nums.size();
 
-        for(int i=0;i<n;i++){
-             bool isSorted=true;
-          for(int j=1;j<n;j++){
-              int first=__builtin_popcount(nums[j-1]);
-            int sec=__builtin_popcount(nums[j]);
-            if(first==sec) if(nums[j]<nums[j-1]) swap(nums[j],nums[j-1]);
-          }
-
-        for(int k=1;k<n;k++){
-            if(nums[k-1]>nums[k]){
-                 isSorted=false;
-                 break;
+        for (int i = 0; i < n; i++) {
+           
+            for (int j = 1; j < n; j++) {
+                int first = __builtin_popcount(nums[j - 1]);
+                int sec = __builtin_popcount(nums[j]);
+                if (first == sec)
+                    if (nums[j] < nums[j - 1])
+                        swap(nums[j], nums[j - 1]);
             }
         }
+         bool isSorted = true;
+         for (int k = 1; k < n; k++) {
+                if (nums[k - 1] > nums[k]) {
+                    isSorted = false;
+                    break;
+                }
+            }
 
-        if(isSorted) return true;
-
-        }
-
-       
+            if (isSorted)
+                return true;
 
         return false;
     }
