fec7543f146b0de142ec7bfd59e414a111686063
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-11-07 08:20:12 +0530

    Time: 42 ms (21.64%), Space: 60.1 MB (74.4%) - LeetHub

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candi) {
+         int maxLen = 0;
+
+        for (int bit = 0; bit < 32; ++bit) {
+            int count = 0;
+            for (int num : candi) {
+                if (num & (1 << bit)) {
+                    count++;
+                }
+            }
+            maxLen = max(maxLen, count);
+        }
+
+        return maxLen;
+    }
+};
