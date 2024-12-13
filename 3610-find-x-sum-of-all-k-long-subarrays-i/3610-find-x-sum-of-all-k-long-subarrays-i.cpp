9378ee150de8da63f826abcb0a6df8ca3c2fbb0a
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2024-12-13 22:58:58 +0530

    Time: 51 ms (5.88%), Space: 38 MB (17.27%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    int solve(vector<int>& temp, int x) {
-        map<int, int> freq;
-        int n = temp.size();
-        for (int i = 0; i < n; i++) {
-            freq[temp[i]]++;
+    int solve(vector<int>temp,int x){
+        int sum=0;
+        unordered_map<int,int>mpp;
+        for(const auto &it:temp){
+            mpp[it]++;
+            sum+=it;
+        }
+        int mapSize=mpp.size();
+        if(mapSize<x) return sum;
+
+        priority_queue<pair<int,int>>pq;
+        for(const auto &[value,freq]:mpp){
+            pq.push({freq,value});
         }
 
-        vector<pair<int, int>> vec;
-        for (auto it = freq.begin(); it != freq.end(); it++) {
-            vec.push_back(make_pair(it->first, it->second));
-        }
-
-        sort(vec.begin(), vec.end(), [](pair<int, int> a, pair<int, int> b) {
-            if (a.second == b.second) {
-                return a.first > b.first;
-            }
-            return a.second > b.second;
-        });
-        int count = 0, sum = 0;
-        for (auto y : vec) {
-            if (count == x)
-                break;
-            sum += (y.first * y.second);
-            count++;
+        sum=0;
+        while(x--){
+            auto  element=pq.top();
+            pq.pop();
+            int value=element.second;
+            int freq=element.first;
+            sum+=(value*freq);
         }
 
         return sum;
     }
     vector<int> findXSum(vector<int>& nums, int k, int x) {
-        int size = nums.size();
-        int ansSize = size - k + 1;
-        vector<int> res;
+        int n=nums.size();
+        vector<int>ans;
 
-         for (int i = 0; i < ansSize; i++) {
-            vector<int> temp(nums.begin() + i, nums.begin() + i + k);
-            int sum = solve(temp, x);
-            res.push_back(sum);
+        for(int i=0;i<=n-k;i++){
+            vector<int>temp(nums.begin()+i,nums.begin()+i+k);
+            int res=solve(temp,x);
+            cout<<"Res value is "<<res<<endl;
+            ans.push_back(res);
         }
 
-        return res;
+        return ans;
     }
 };
