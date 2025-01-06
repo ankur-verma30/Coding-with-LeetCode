d38ac03c2c47b7651b9a494a1cfdf79049e5e322
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-01-06 11:07:19 +0530

    Time: 28 ms (35.42%), Space: 46.5 MB (9.74%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,17 +2,9 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        if (amount == 0)
-            return 1;
-        if (n == 0)
-            return 0;
-        vector<vector<uint64_t>> dp(n + 2, vector<uint64_t>(amount + 2, -1));
+        vector<vector<uint64_t>> dp(n + 1, vector<uint64_t>(amount + 1, 0));
         for (int i = 0; i < n + 1; i++) {
             dp[i][0] = 1;
-        }
-
-        for (int i = 0; i < amount + 1; i++) {
-            dp[0][i] = 0;
         }
 
         for (int i = 1; i < n + 1; i++) {
