a26c0ca4d234826654270269f043a8724975666a
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-01-08 11:41:34 +0530

    Time: 4 ms (52.97%), Space: 23.6 MB (59.11%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -7,14 +7,11 @@ public:
             string word1=words[i];
             int l1=word1.length();  
             for(int j=i+1;j<l0;j++){
-                  bool prefix=false,suffix=false;
-                string word2=words[j];
+                string word2=words[j];  
                 int l2=word2.length();
-                
+                if(l2<l1) continue;
                 if(l2>=l1){
-                 if(word1==word2.substr(0,l1)) prefix=true;
-                    if(word1==word2.substr(l2-l1)) suffix=true;
-                    if(prefix && suffix) count++;
+                 if(word1==word2.substr(0,l1) && (word1==word2.substr(l2-l1))) count++; 
                 }
                 
             }
