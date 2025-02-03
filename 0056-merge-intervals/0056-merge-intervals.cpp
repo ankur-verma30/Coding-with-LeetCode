c8689e0d85889196fb011e69cf5967b1701de3ef
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-02-03 09:19:16 +0530

    Time: 3 ms (90.06%), Space: 23.8 MB (58.18%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,23 +5,22 @@ public:
         vector<vector<int>> ans;
 
         sort(intervals.begin(), intervals.end());
+        int first=intervals[0][0],second=intervals[0][1]; 
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             int start = intervals[i][0];
             int end = intervals[i][1];
 
-            if (!ans.empty() && end <= ans.back()[1])
-                continue;
-
-            for (int j = i + 1; j < n; j++) {
-                if (intervals[j][0] <= end) {
-                    end = max(end, intervals[j][1]);
-                } else
-                    break;
+            if (start<=second){
+                second=max(second,end);
             }
-            ans.push_back({start, end});
-        }
-
+            else{
+                ans.push_back({first,second});
+                first=start;
+                second=end;
+            }
+        }    
+        ans.push_back({first,second});
         return ans;
     }
 };
