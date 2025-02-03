fac77961451048228615e8b8a6f038e40a646007
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-02-03 17:18:46 +0530

    Time: 31 ms (98.54%), Space: 101.9 MB (22.74%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -6,7 +6,6 @@ public:
 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
 
-        //solve n-meeting in one room first
         int n=intervals.size();
         int count=0;
 
@@ -18,17 +17,11 @@ public:
         sort(interval.begin(),interval.end(),compare);
         int freeTime=interval[0].second;
 
-        for(int i=0;i<n;i++){
-            cout<<"{ "<<interval[i].first<<", "<<interval[i].second<<" }"<<endl;
-        }
-
         for(int i=1;i<n;i++){
             int start=interval[i].first;
             int end=interval[i].second;
             if(freeTime>start) count++;
             else freeTime=end;
-            
-            cout<<"free time is"<<freeTime<<endl;
         }
 
         return count;
