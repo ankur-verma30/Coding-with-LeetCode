da3c10a56160e5639a150f1667de9c88a5337454
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-02-16 18:28:47 +0530

    Time: 287 ms (29.41%), Space: 15.4 MB (94.12%) - LeetHub

--- 3771-select-k-disjoint-special-substrings/3771-select-k-disjoint-special-substrings.cpp
+++ 3771-select-k-disjoint-special-substrings/3771-select-k-disjoint-special-substrings.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-  bool maxSubstringLength(string s, int k) {
-    int n = s.size();
-    unordered_map<char, int> firstOccurrence, lastOccurrence;
-    vector<pair<int, int>> intervals;
+    bool maxSubstringLength(string s, int k) {
+        int n=s.length();
+        if(k>n) return false;
+       unordered_map<int,int>firstOccurance,lastOccurance;
 
-    for (int i = 0; i < n; i++) {
-      if (!firstOccurrence.count(s[i])) firstOccurrence[s[i]] = i;
-      lastOccurrence[s[i]] = i;
-    }
+       vector<pair<int,int>>intervals;
 
-    for (int i = 0; i < n; i++) {
-      if (i != firstOccurrence[s[i]]) continue;
-      int left = firstOccurrence[s[i]], right = lastOccurrence[s[i]];
-      bool isValid = true;
-      for (int j = left; j <= right; j++) {
-        right = max(right, lastOccurrence[s[j]]);
-        if (firstOccurrence[s[j]] < left) isValid = false;
-      }
-      if (right - left + 1 < n && isValid) intervals.push_back({left, right});
-    }
+       for(int i=0;i<n;i++){
+        if(firstOccurance.find(s[i])==firstOccurance.end()) firstOccurance[s[i]]=i;
+        lastOccurance[s[i]]=i;
+       } 
 
-    sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
-            return a.second < b.second;
+       for(int i=0;i<n;i++){
+        if(i!=firstOccurance[s[i]]) continue;
+        int left=firstOccurance[s[i]];
+        int right=lastOccurance[s[i]];
+        bool isValid=true;
+        for(int index=left;index<=right;index++){
+            right=max(right,lastOccurance[s[index]]);
+            if(firstOccurance[s[index]]<left) isValid=false;
+        }
+        if(right-left+1<n && isValid) intervals.push_back({left,right});
+       }
+
+        sort(intervals.begin(),intervals.end(),[](const auto &a,const auto &b){
+            return a.second<b.second; 
         });
 
-    int count = 0, lastEnd = -1;
-    for (auto &[start, end] : intervals) {
-      if (start > lastEnd) {
-        count++;
-        lastEnd = end;
-      }
+        int count=0,lastEnd=-1;
+
+        for(const auto &[start,end]:intervals ){
+            if(start>lastEnd){
+                count++;
+                lastEnd=end;
+            }
+        }
+        return count>=k ? true : false;
+
+
+       
     }
-    
-    return (count >= k);
-  }
 };
