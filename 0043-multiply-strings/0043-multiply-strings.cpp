23d4d34ef03fd6650107f25c743bccb1efc3c5c1
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-02-21 13:46:41 +0530

    Time: 50 ms (9.07%), Space: 13.5 MB (16.62%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -4,13 +4,12 @@ public:
         if(ans.empty()) return str;
         int n1=ans.length(),n2=str.length();
         string res;
-        int i=n1-1,j=n2-1,carry=0;
+        int i=n1-1,j=n2-1,carry=0,sum=0,rem=0;
         while(i>=0 && j>=0){
             int val1=ans[i]-'0';
             int val2=str[j]-'0';
-            int sum=val1+val2+carry;
-            int rem=sum%10;
-            cout<<"In function the value of rem "<<rem<<endl;
+            sum=val1+val2+carry;
+            rem=sum%10;
             res+=to_string(rem);
             carry=sum/10;
             i--;
@@ -18,19 +17,17 @@ public:
         }
 
         while(i>=0){
-            int sum=(ans[i]-'0')+carry;
-            int temp=sum%10;
-             cout<<"In function the value of temp from ans "<<temp<<endl;
-            res+=to_string(temp);
+            sum=(ans[i]-'0')+carry;
+            rem=sum%10;
+            res+=to_string(rem);
             carry=sum/10;
             i--;
         }
 
         while(j>=0){
-            int sum=(str[j]-'0')+carry;
-            int temp=sum%10;
-             cout<<"In function the value of temp from str "<<temp<<endl;
-            res+=to_string(temp);
+            sum=(str[j]-'0')+carry;
+            rem=sum%10;
+            res+=to_string(rem);
             carry=sum/10;
             j--;
         }
@@ -68,15 +65,11 @@ public:
             
         }
         int temp=count;
-        cout<<"Value of count "<<count<<endl;
         if(carry!=0) str+=to_string(carry);
         reverse(str.begin(),str.end());
-        cout<<"Value of the iteration "<<i<< " value of string str "<<str<<endl;
         while(temp--) str+='0';
-        cout<<"value of str "<<str<<endl;
         count++;
         ans=AddTwoString(ans,str);
-        cout<<"ans after the "<<i<<"th iteration "<<ans<<endl;
        }
 
        if(carry!=0)ans=to_string(carry)+ans;
