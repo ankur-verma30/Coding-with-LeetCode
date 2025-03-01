97991507a5ec09da05841dba0b4b0eb9f250ca42
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-03-02 00:55:45 +0530

    Time: 4 ms (7.69%), Space: 28.4 MB (7.69%) - LeetHub

---
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]%2) nums[i]=1;
+            else nums[i]=0;
+        }
+
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
