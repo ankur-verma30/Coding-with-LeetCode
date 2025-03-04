a8eb13c026df941552dfc95233a66c74e83020c4
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-03-04 23:51:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (9.62%) - LeetHub

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void powerOfThree(int n,vector<int>&power){
+       int i=1;
+       while(i<=n){
+        power.push_back(i);
+        i=i*3;
+       }
+    }
+    bool checkPowersOfThree(int n) {
+       vector<int> power;
+        powerOfThree(n, power);
+        int len = power.size() - 1;
+
+        while (len >= 0 && n > 0) {
+            if (power[len] == n) {
+                return true;
+            } else if (power[len] < n) {
+                n -= power[len];
+            }
+            len--;
+        }
+
+        return n == 0;
+    }
+};
