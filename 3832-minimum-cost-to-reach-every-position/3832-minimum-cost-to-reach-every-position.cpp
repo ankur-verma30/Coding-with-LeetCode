0159072f29a5727876d164f4b325cbf5a755d0d6
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-03-30 14:34:11 +0530

    Time: 0 ms (100%), Space: 26.7 MB (40.81%) - LeetHub

--- 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
+++ 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
@@ -5,9 +5,7 @@ public:
 
         vector<int>ans(n,0);
         for(int i=0;i<n;i++){
-            if(cost[i]>=smallestNumber){
-                 ans[i]=smallestNumber;
-            }
+            if(cost[i]>=smallestNumber) ans[i]=smallestNumber;
             else if(smallestNumber>cost[i]){
                 ans[i]=cost[i];
                 smallestNumber=min(smallestNumber,cost[i]);
