5d3d975181bc9e46823628062b8349001f9efb52
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-04-06 12:28:28 +0530

    Time: 372 ms (37.09%), Space: 440.3 MB (30.98%) - LeetHub

--- 3827-implement-router/3827-implement-router.cpp
+++ 3827-implement-router/3827-implement-router.cpp
@@ -1,6 +1,5 @@
 class Router {
 private:
-
     int memoryLimit;
     queue<vector<int>> queue;
     unordered_set<string> packetSet;
@@ -10,45 +9,47 @@ public:
     Router(int memoryLimit) : memoryLimit(memoryLimit) {}
 
     bool addPacket(int source, int destination, int timestamp) {
-        string tempHash=to_string(source)+"-"+
-        to_string(destination)+"-"+to_string(timestamp);
+        string tempHash = to_string(source) + "-" + to_string(destination) +
+                          "-" + to_string(timestamp);
 
-        if (packetSet.count(tempHash)) return false;
+        if (packetSet.count(tempHash))
+            return false;
 
         if (queue.size() >= memoryLimit) {
-            vector<int>temp= queue.front();
+            vector<int> temp = queue.front();
             queue.pop();
-            string canHash=to_string(temp[0])+"-"+
-        to_string(temp[1])+"-"+to_string(temp[2]);
+            string canHash = to_string(temp[0]) + "-" + to_string(temp[1]) +
+                             "-" + to_string(temp[2]);
             packetSet.erase(canHash);
-            destinationTimestamps[temp[1]].
-                erase(destinationTimestamps[temp[1]].begin());
+            destinationTimestamps[temp[1]].erase(
+                destinationTimestamps[temp[1]].begin());
         }
 
-        queue.push({source,destination,timestamp});
+        queue.push({source, destination, timestamp});
         packetSet.insert(tempHash);
         destinationTimestamps[destination].push_back(timestamp);
         return true;
     }
 
     vector<int> forwardPacket() {
-        if (queue.empty()) return {};
+        if (queue.empty())
+            return {};
 
         vector<int> packet = queue.front();
         queue.pop();
-         string canHash=to_string(packet[0])+"-"+
-        to_string(packet[1])+"-"+to_string(packet[2]);
+        string canHash = to_string(packet[0]) + "-" + to_string(packet[1]) +
+                         "-" + to_string(packet[2]);
         packetSet.erase(canHash);
-        destinationTimestamps[packet[1]].
-            erase(destinationTimestamps[packet[1]].begin());
+        destinationTimestamps[packet[1]].erase(
+            destinationTimestamps[packet[1]].begin());
         return {packet[0], packet[1], packet[2]};
     }
 
     int getCount(int destination, int startTime, int endTime) {
-        auto& timestamps = destinationTimestamps[destination];
-        return count_if(timestamps.begin(), timestamps.end(), [&](int t) {
-            return t >= startTime && t <= endTime;
-        });
+        auto& vec = destinationTimestamps[destination];
+        auto low = lower_bound(vec.begin(), vec.end(), startTime);
+        auto high = upper_bound(vec.begin(), vec.end(), endTime);
+        return high - low;
     }
 };
 /**
