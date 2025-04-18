d0cdc511021a75f253ccae6b141403b97411593f
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-04-18 14:40:24 +0530

    Time: 5 ms (41.53%), Space: 9.9 MB (72.29%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string str="1";
+        string str = "1";
 
-        for(int i=1;i<n;i++){
-            unordered_map<char,int>mpp;
-            int count =0;
-            char prev=str[0];
+        for (int i = 1; i < n; i++) {
+            unordered_map<char, int> mpp;
+            int count = 0;
+            char prev = str[0];
             string temp;
-            for(int i=0;i<str.length();i++){
-                    if(prev==str[i]) count++;
-                    else if(prev!=str[i]){
-                        temp+=to_string(count)+prev;
-                        count=1;
-                        prev=str[i];
-                    }
+            for (int i = 0; i < str.length(); i++) {
+                if (prev == str[i])
+                    count++;
+                else if (prev != str[i]) {
+                    temp += to_string(count) + prev;
+                    count = 1;
+                    prev = str[i];
+                }
             }
-             temp+=to_string(count)+prev;
-            cout<<"The value of temp is "<<temp<<endl;
-            str=temp;
+            temp += to_string(count) + prev;
+            str = temp;
         }
         return str;
     }
