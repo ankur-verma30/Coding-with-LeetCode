d5b5ee411f40c1adc57dd116aad4c2fdd78045a1
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-04-20 18:37:14 +0530

    Time: 0 ms (100%), Space: 206.2 MB (91.92%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
 
-        int largest=nums[0];
-        vector<int>res;
-        for(int i=1;i<n;i++){
-            if(nums[i]>=largest){
-                res.push_back(largest);
-                largest=nums[i];
+        int largest = nums[0],count=1;
+       
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= largest) {
+                count++;
+                largest = nums[i];
             }
         }
 
-        res.push_back(largest);
-        return res.size();
-
+        return count;
     }
 };
