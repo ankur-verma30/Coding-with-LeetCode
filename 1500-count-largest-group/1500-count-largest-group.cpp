2194d3eb1dfda50346759b70f1f29e70dda918a1
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-04-25 02:07:52 +0530

    Time: 2 ms (56.14%), Space: 10.5 MB (6.52%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.cpp
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int sumOfDigits(int n){
-        int sum=0;
-        while(n>0){
-            sum+=n%10;
-            n=n/10;
+    int sumOfDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = n / 10;
         }
         return sum;
     }
 
     int countLargestGroup(int n) {
-        vector<int>freq(n+1,0);
+        vector<int> freq(n + 1, 0);
 
-        for(int i=1;i<=n;i++){
-            int sum=sumOfDigits(i);
+        for (int i = 1; i <= n; i++) {
+            int sum = sumOfDigits(i);
             freq[sum]++;
         }
 
+        auto maxi = *max_element(freq.begin(), freq.end());
+        int count = 0;
 
-        auto maxi= *max_element(freq.begin(),freq.end());
-        int count=0;
-
-        for(const auto it:freq) {
-            if(it==maxi) count++;
+        for (const auto it : freq) {
+            if (it == maxi)
+                count++;
         }
 
         return count;
