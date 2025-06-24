7bbf03020f2fe20020a9b01d997a193dc8215f8a
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-06-24 21:24:16 +0530

    Time: 4 ms (55.96%), Space: 14.7 MB (73.39%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        int n=nums.size();
+        int n = nums.size();
 
-        vector<int>ans;
+        vector<int> ans;
 
-        for(int i=0;i<n;i++){
-            if(nums[i]==key){
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
                 ans.push_back(i);
                 continue;
             }
-            bool isFound=false;
-            for(int j=0;j<n;j++){
-                if(nums[j]!=key) continue;
-                else if(abs(i-j)<=k) {
-                    isFound=true; 
+            bool isFound = false;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] != key)
+                    continue;
+                else if (abs(i - j) <= k) {
+                    ans.push_back(i);
                     break;
-                }  
+                }
             }
-            if(isFound) ans.push_back(i);
         }
 
         return ans;
