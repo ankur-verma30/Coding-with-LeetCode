c8dfc68f5ef0c0c68eca52a2ae2565cc9d572cd0
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-07-19 23:27:49 +0530

    Time: 60 ms (62.37%), Space: 51.4 MB (63.44%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,70 +1,30 @@
-class Trie {
-    Trie* childrenNode[27];
-    bool isEndsWith;
-
-public:
-    Trie() {
-        isEndsWith = false;
-        for (int i = 0; i < 27; i++)
-            childrenNode[i] = NULL;
-    }
-
-    ~Trie() {
-        for (int i = 0; i < 27; i++)
-            delete childrenNode[i];
-    }
-
-    int getIndex(char ch) { return ch == '/' ? 26 : ch - 'a'; }
-
-    void insertKey(const string& key) {
-        Trie* curr = this;
-        for (auto ch : key) {
-            int index = getIndex(ch);
-            if (curr->childrenNode[index] == NULL)
-                curr->childrenNode[index] = new Trie();
-
-            curr = curr->childrenNode[index];
-        }
-        curr->childrenNode[26] = new Trie();
-        curr = curr->childrenNode[26];
-        curr->isEndsWith = true;
-    }
-
-    bool searchKey(const string& folder) {
-        Trie* curr = this;
-        for (auto ch : folder) {
-            if (curr->isEndsWith)
-                return true;
-
-            int index = getIndex(ch);
-            if (curr->childrenNode[index] == NULL)
-                return false;
-
-            curr = curr->childrenNode[index];
-        }
-        return curr->isEndsWith;
-    }
-};
 class Solution {
 public:
-    static bool cmp(const string& a, const string& b) {
-        return a.length() < b.length();
-    }
-
     vector<string> removeSubfolders(vector<string>& folder) {
-        Trie* root = new Trie();
-        int n = folder.size();
-
-        sort(folder.begin(), folder.end(), cmp);
+        sort(folder.begin(), folder.end());
+        unordered_set<string> st;
         vector<string> ans;
-        for (int i = 0; i < n; i++) {
-            string str = folder[i];
-            if (!root->searchKey(str)) {
-                root->insertKey(str);
-                ans.push_back(str);
+
+        for (const string& path : folder) {
+            string temp;
+            bool isSubfolder = false;
+
+            for (int i = 1; i < path.size(); ++i) {
+                if (path[i] == '/') {
+                    temp = path.substr(0, i);
+                    if (st.count(temp)) {
+                        isSubfolder = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isSubfolder) {
+                st.insert(path);
+                ans.push_back(path);
             }
         }
-        delete root;
+
         return ans;
     }
 };
