b3be492ab99d3e1ec76b518d1cb2dc406daf645d
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-07-25 11:33:28 +0530

    Time: 0 ms (100%), Space: 58.1 MB (70.11%) - LeetHub

---
+++ 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumGroups(int[] grades) {
+        int n = grades.length, low = 0, high = 1000, ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((mid * (mid + 1)) / 2 <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+}
