f0041e1159d4c20097ff2d7477c099697e9a6945
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-08-11 12:13:47 +0530

    Time: 4 ms (15.65%), Space: 9.9 MB (8.99%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -5,22 +5,24 @@ public:
             return true;
 
         string str = to_string(n);
-        sort(str.rbegin(),str.rend());
+        sort(str.rbegin(), str.rend());
         map<char, int> mp;
         for (const auto& it : str)
             mp[it]++;
 
-        n=stoi(str);   
+        n = stoi(str);
 
-        int i=0;
-        while((1 << i) <= n) {
-            map<char, int> temp;
+        int i = 0;
+        map<char, int> temp;
+        while ((1 << i) <= n) {
             int num = (1 << i);
             string st = to_string(num);
             for (const auto& it : st)
                 temp[it]++;
-            if(temp==mp) return true;
+            if (temp == mp)
+                return true;
             i++;
+            temp.clear();
         }
         return false;
     }
