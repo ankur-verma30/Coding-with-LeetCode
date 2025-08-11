4635a8cf485680bf71a22fba940520bd85d233f9
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-08-11 21:36:45 +0530

    Time: 94 ms (14.89%), Space: 165.3 MB (27.66%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> powerList(int n) {
         int i = 30;
         vector<int> ans;
-        while (n > 0 && i>=0) {
+        while (n > 0 && i >= 0) {
             int val = (1 << i);
             if (val <= n) {
                 ans.push_back(val);
@@ -16,18 +16,22 @@ public:
     }
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
         vector<int> powers = powerList(n);
+
         int size = powers.size();
         vector<int> result;
+
         int mod = 1e9 + 7;
+
         for (const auto query : queries) {
             int start = query[0];
             int end = query[1];
-            if (end >= size)
-                end = size-1;
-            long long  pro = 1;
+
+            long long pro = 1;
+
             for (int i = start; i <= end; i++) {
                 pro = (pro * powers[i]) % mod;
             }
+
             result.push_back((int)pro);
         }
         return result;
