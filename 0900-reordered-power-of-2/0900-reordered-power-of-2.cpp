233d7f76ff94a8504eb2f24e1d2a3c86fa7259fc
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-08-11 12:11:08 +0530

    Time: 4 ms (15.65%), Space: 9.8 MB (10.72%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        if((n &(n-1))==0) return true;
+        if ((n & (n - 1)) == 0)
+            return true;
 
-        string str=to_string(n);
-        sort(str.begin(),str.end());
+        string str = to_string(n);
+        sort(str.rbegin(),str.rend());
+        map<char, int> mp;
+        for (const auto& it : str)
+            mp[it]++;
 
-            do{
-            if(str[0]=='0') continue;
-            int num=stoi(str);
-            cout<<num<<'\n';
-             if((num &(num-1))==0) return true;
-        }while(next_permutation(str.begin(),str.end()));
+        n=stoi(str);   
 
+        int i=0;
+        while((1 << i) <= n) {
+            map<char, int> temp;
+            int num = (1 << i);
+            string st = to_string(num);
+            for (const auto& it : st)
+                temp[it]++;
+            if(temp==mp) return true;
+            i++;
+        }
         return false;
     }
 };
