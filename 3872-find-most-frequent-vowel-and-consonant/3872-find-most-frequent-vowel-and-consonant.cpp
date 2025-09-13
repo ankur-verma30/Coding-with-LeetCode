cf9f2957408a2badb419560ff4996ae05a135234
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-09-13 10:24:58 +0530

    Time: 3 ms (37.49%), Space: 10.2 MB (12.19%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -4,11 +4,9 @@ public:
         unordered_map<char, int> freq;
         unordered_set<char> st{'a', 'e', 'i', 'o', 'u'};
 
-        for (char ch : s)
-            freq[ch]++;
-
         int vowelFreq = 0, consonantFreq = 0;
-        for (const auto& [character, occurance] : freq) {
+        for (char &character:s) {
+            freq[character]++;
             if (st.count(character))
                 vowelFreq = max(vowelFreq, freq[character]);
             else
