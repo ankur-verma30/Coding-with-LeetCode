0ebfd17d3c18149e6123be6e92b83e0cafba43ba
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-09-15 09:01:19 +0530

    Time: 0 ms (100%), Space: 9 MB (57.81%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        vector<string>words;
-        string str="";
-        for(int i=0;i<text.length();i++){
-            if(text[i]==' '){
-                words.push_back(str);
-                str="";
-            }
-            str+=text[i];
-        }
-
-        words.push_back(str);
-
         unordered_set<char>st;
         for(const auto ch:brokenLetters) st.insert(ch);
 
         int count=0;
-        for(const auto &word:words ){
-            bool flag=true;
-            for(const auto ch:word){
-                if(st.count(ch)){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag) count++;
+        bool isBroken=false;
+        for(int i=0;i<text.length();i++){
+           if(text[i]!=' '){
+            if(st.count(text[i])) isBroken=true;
+           }
+           else{
+            if(!isBroken) count++;
+            isBroken=false;
+           }
         }
-
+        
+        if(!isBroken) count++;
         return count;
     }
 };
