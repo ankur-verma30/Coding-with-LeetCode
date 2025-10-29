10e717a2fd6ae60f4236bc56a62674bb5ef64c34
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-10-29 06:05:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (89.59%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -4,14 +4,13 @@ public:
     int smallestNumber(int n) {
         string bits;
 
-        while(n>0){
-            int val=(n&1);
-            bits+=(val+'0');
-            n=n>>1;
+        while (n > 0) {
+            int val = (n & 1);
+            bits += (val + '0');
+            n = n >> 1;
         }
-        int len=bits.length();
-
-        return (1<<len)-1;
-
+        
+        int len = bits.length();
+        return (1 << len) - 1;
     }
 };
