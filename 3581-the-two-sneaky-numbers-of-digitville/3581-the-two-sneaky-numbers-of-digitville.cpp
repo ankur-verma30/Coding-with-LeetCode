3047d96eec4c9522973c3870e164e97930c9f022
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-10-31 07:30:00 +0530

    Time: 0 ms (100%), Space: 25.7 MB (63.41%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        vector<int>freq(100,0);
-        vector<int>ans;
+        vector<int> freq(100, 0);
+        vector<int> ans;
 
-
-        for(const auto it:nums){
-            if(freq[it]) ans.push_back(it);
-            else freq[it]=1;
-
+        for (const auto it : nums) {
+            if (freq[it])
+                ans.push_back(it);
+            else
+                freq[it] = 1;
         }
         return ans;
     }
