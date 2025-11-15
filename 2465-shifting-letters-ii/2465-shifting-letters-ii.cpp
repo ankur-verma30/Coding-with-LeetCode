026148223ecd54c47e6a93a97d86181bf03561e7
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-11-15 15:34:57 +0530

    Time: 48 ms (21.91%), Space: 109.2 MB (6.6%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
+    char nthNextChar(char c, int n) {
+        int index = c - 'a';
+        int newIndex = (index + n) % 26;
+        if (newIndex < 0)
+            newIndex += 26;
+        return 'a' + newIndex;
+    }
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.length();
-        vector<int> prefix(n + 1, 0);
+        vector<int> difference(n, 0);
 
-        for (const auto query : shifts) {
-            int start = query[0];
-            int end = query[1];
-            int direction = query[2];
+        for (const auto shift : shifts) {
+            int start = shift[0];
+            int end = shift[1];
+            int value = (shift[2] == 0) ? -1 : 1;
 
-            if (direction == 0) {
-                prefix[start] += -1;
-                prefix[end + 1] += 1;
-            } else {
-                prefix[start] += 1;
-                prefix[end + 1] += -1;
-            }
+            difference[start] += value;
+            if (end + 1 < n)
+                difference[end + 1] -= value;
         }
 
-        string str = s;
-        int shiftSum = 0;
         for (int i = 0; i < n; i++) {
-            shiftSum =(shiftSum+prefix[i])%26;
-            if(shiftSum<0) shiftSum+=26;
-            str[i] = 'a' + ((str[i] - 'a') + shiftSum) % 26;
+            if (i == 0)
+                continue;
+            difference[i] += difference[i - 1];
+        }
+
+        string str = "";
+        for (int i = 0; i < n; i++) {
+            char ch = nthNextChar(s[i], difference[i]);
+            str += ch;
         }
 
         return str;
