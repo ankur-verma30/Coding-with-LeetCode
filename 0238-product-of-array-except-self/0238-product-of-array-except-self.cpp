275007c17124cc5f99af9ff9c15bc848107b3a3c
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-11-18 20:30:03 +0530

    Time: 7 ms (6.01%), Space: 40.1 MB (94.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& arr) {
         long long pro = 1;
-        bool isZero = false;
         int zeroCount = 0;
         int n = arr.size();
+
         for (int i = 0; i < n; i++) {
             if (arr[i] == 0) {
-                isZero = true;
                 zeroCount++;
                 continue;
-
             } else
                 pro *= arr[i];
         }
 
         vector<int> res(n);
         for (int i = 0; i < n; i++) {
-            if (isZero) {
+            if (zeroCount) {
                 if (arr[i] != 0)
                     res[i] = 0;
                 else if (arr[i] == 0 && zeroCount <= 1)
