8f66ce8a3addf645e89209c6e53311404904d4b5
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-11-19 11:54:40 +0530

    Time: 18 ms (7.38%), Space: 14 MB (8.12%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,78 +1,51 @@
 class Solution {
 public:
-    string AddTwoString(string& ans, string str) {
-        if (ans.empty())
-            return str;
-        int n1 = ans.length(), n2 = str.length();
-        string res;
-        int i = n1 - 1, j = n2 - 1, carry = 0, sum = 0, rem = 0;
-        while (i >= 0 && j >= 0) {
-            sum =(ans[i]-'0') +(str[j]-'0') + carry;
-            rem = sum % 10;
-            res += to_string(rem);
+
+    string addString(string a, string b) {
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+        string ans;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            ans += (sum % 10) + '0';
             carry = sum / 10;
-            i--;
-            j--;
         }
 
-        while (i >= 0) {
-            sum = (ans[i] - '0') + carry;
-            rem = sum % 10;
-            res += to_string(rem);
-            carry = sum / 10;
-            i--;
-        }
-
-        while (j >= 0) {
-            sum = (str[j] - '0') + carry;
-            rem = sum % 10;
-            res += to_string(rem);
-            carry = sum / 10;
-            j--;
-        }
-
-        if (carry == 1)
-            res += '1';
-        reverse(res.begin(), res.end());
-        return res;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 
     string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
 
-        if (num1 == "0" || num2 == "0")
-            return "0";
-        int n1 = num1.length(), n2 = num2.length();
+        int n1 = num1.size(), n2 = num2.size();
+        string ans = "0";
 
-        if (n1 < n2) {
-            swap(num1, num2);
-            swap(n1, n2);
-        }
+        for (int i = n1 - 1; i >= 0; i--) { // RIGHT to LEFT
+            int carry = 0, val1 = num1[i] - '0';
+            string temp;
 
-        string ans;
-        int count = 0, carry = 0;
+            // shift zeros based on position
+            temp.append(n1 - 1 - i, '0');
 
-        for (int i = n2 - 1; i >= 0; i--) {
-            int multiplier = num2[i] - '0', carry = 0;
-            string str;
-            for (int j = n1 - 1; j >= 0; j--) {
-                int multiplicand = num1[j] - '0';
-                int product = multiplier * multiplicand + carry;
-                int rem = product % 10;
-                carry = product / 10;
-                str += to_string(rem);
+            for (int j = n2 - 1; j >= 0; j--) {
+                int val2 = num2[j] - '0';
+                int pro = val1 * val2 + carry;
+                temp += (pro % 10) + '0';
+                carry = pro / 10;
             }
-            int temp = count;
-            if (carry != 0)
-                str += to_string(carry);
-            reverse(str.begin(), str.end());
-            while (temp--)
-                str += '0';
-            count++;
-            ans = AddTwoString(ans, str);
+
+            if (carry) temp += carry + '0';
+
+            reverse(temp.begin(), temp.end());
+            ans = addString(ans, temp);
         }
 
-        if (carry != 0)
-            ans = to_string(carry) + ans;
         return ans;
     }
 };
