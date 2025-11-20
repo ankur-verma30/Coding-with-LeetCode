63e8ce4b054337e5a54b10c9d49e58cc3f0e1fdf
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-11-20 12:15:33 +0530

    Time: 0 ms (100%), Space: 55 MB (59.71%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-    
+        bool isOnePresent=false;
+        int n=nums.size();
 
-         //Brute force Method T.C O(2N)  S.C.O(N)
-
-         map<int,int>counter;
-
-    for(int i=0; i<nums.size();i++)
-        counter[nums[i]]++;
-
-        int count=1;
-    for(int i=0;i<nums.size();i++){
-            if(counter[count]==0)
-            return count;
-            else 
-            count++;
+        for(const auto num:nums){
+            if(num==1){
+                isOnePresent=true;
+                break;
+            }
         }
-        return count;
-        
 
-        //We have to solve in T.C. O(N)  and S.C  O(1)
-        
+        if(!isOnePresent) return 1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<1 || nums[i]>n) nums[i]=1;
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[abs(nums[i])-1]>0) nums[abs(nums[i])-1]*=-1;
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0) return i+1;
+        }
+
+        return n+1;
     }
 };
