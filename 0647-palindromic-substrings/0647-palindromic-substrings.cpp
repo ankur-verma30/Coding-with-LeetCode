051e34e5bc1f27402058ccbf415761b0f472988d
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-11-26 11:19:39 +0530

    Time: 27 ms (32.79%), Space: 34.6 MB (18.01%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    bool checkPalindrome(int start, int end, string& s,vector<vector<int>>&dp) {
-        if (start >= end)
-            return true;
-
-            if(dp[start][end]!=-1) return dp[start][end];
-        if (s[start] != s[end]) return dp[start][end]= false;
-        return dp[start][end]=checkPalindrome(start + 1, end - 1, s,dp);
-
-    }
     int countSubstrings(string s) {
-        int n = s.length();
-        int count = 0;
+        int n=s.length();
 
         vector<vector<int>>dp(n,vector<int>(n,-1));
+        int count=0;
 
-        // dp[i][j]==0 then false
-        // dp[i][j]==1 then true
-        // dp[i][j]==-1 then not solved
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (checkPalindrome(i, j, s,dp))
-                    count++;
+        for(int length=1;length<=n;length++){
+            for(int start=0;start+length-1<n;start++){
+                int end=start+length-1;
+                if(start==end) dp[start][end]=1;
+                else if(start+1==end && s[start]==s[end]) 
+                dp[start][end]=1;
+                else {
+                    if(s[start]==s[end] && dp[start+1][end-1]==1) 
+                    dp[start][end]=1;
+                }
+                if(dp[start][end]==1) count++;
             }
         }
 
