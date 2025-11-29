3a4333bbad59d519cdaadf003dde9137e824ae47
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-11-29 13:21:37 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int j = 0;
 
-        //Not the optimal solution operation can be reduced 
-        reverse(nums.begin(),nums.end());
-        stack<int> st;
-        int count=0;
-
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0)
-            count++;
-            else
-            st.push(nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
-
-        for(int i=0;i<nums.size();i++)
-        if(!st.empty()){
-            nums[i]=st.top();
-            st.pop();
-        }
-        else if(count!=0){
-            nums[i]=0;
-            count--;
-        }
-        
     }
 };
