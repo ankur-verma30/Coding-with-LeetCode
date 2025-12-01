dd58c8f52d84be581b21de3ce2df7a5db13e3010
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-12-01 09:50:47 +0530

    Time: 0 ms (100%), Space: 13.7 MB (88.58%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-     void digits(char ch, int& start, vector<char>& chars, int freqlen) {
-        string freqStr = to_string(freqlen);
-        chars[start++] = ch;
-        for (char digit : freqStr) {
-            chars[start++] = digit;
-        }
-    }
-
     int compress(vector<char>& chars) {
-        int n = chars.size(), freq=0;
-        char ch=chars[0];
-        int start=0;
-        for(int i=0;i<n;i++){
-            if(chars[i]==ch) freq++;
-            else{
-                 if (freq == 1) chars[start++] = ch;
-             else  digits(ch, start, chars, freq);
-                ch=chars[i];
-                freq=1;
+        char prev = chars[0];
+        int count=1, n = chars.size(), index = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (chars[i] == prev) {
+                count++;
+                continue;
+            } else {
+                if (count != 1) {
+                    string str = to_string(count);
+                    for (const auto it : str)
+                        chars[index++] = it;
+                }
+                count=0;
+                prev = chars[i];
+                chars[index++]=prev;
+                count=1;
             }
         }
+        if (count != 1) {
+        string str = to_string(count);
+        for (const auto it : str)
+        chars[index++] = it;
+                }
 
-          if (freq == 1) {
-            chars[start++] = ch;
-        } else {
-            digits(ch, start, chars, freq);
-        }
-
-        chars.resize(start);
-        return chars.size();
+        return index;
     }
-
 };
