cc27babcb64ab192e01ef83033007bda69ec7474
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-12-09 22:31:08 +0530

    Time: 322 ms (95.62%), Space: 280.3 MB (96.35%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-       unordered_map<int,int>validPair;
-       unordered_map<int,int>leftVisited;
+        unordered_map<int, int> validPair;
+        unordered_map<int, int> leftVisited;
 
-       int count=0,n=nums.size(),mod=1e9+7;
+        int count = 0, n = nums.size(), mod = 1e9 + 7;
 
-       for(int i=0;i<n;i++){
-        if(nums[i]%2==0 && validPair.count(nums[i]/2)){
-            int freq=validPair[nums[i]/2]%mod;
-            count=(count%mod+freq%mod)%mod;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0 && validPair.count(nums[i] / 2)) {
+                int freq = validPair[nums[i] / 2] ;
+                count = (count % mod + freq % mod) % mod;
+            }
+            if (leftVisited.count(nums[i] * 2)) {
+                int doubleFreq = leftVisited[nums[i] * 2] ;
+                validPair[nums[i]] =
+                    (validPair[nums[i]] % mod + doubleFreq % mod) % mod;
+            }
+            leftVisited[nums[i]]++;
         }
-        if(leftVisited.count(nums[i]*2)){
-            int doubleFreq=leftVisited[nums[i]*2]%mod;
-            validPair[nums[i]]=(validPair[nums[i]]%mod+doubleFreq%mod)%mod;
-        }
-        leftVisited[nums[i]]++;
-       } 
 
-       return count;
+        return count;
     }
 };
