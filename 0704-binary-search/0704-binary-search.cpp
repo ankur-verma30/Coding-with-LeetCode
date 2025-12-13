5641d10ee9ce6b8c7ba0d970a2c6a99a158a272c
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2025-12-13 08:38:38 +0530

    Time: 0 ms (100%), Space: 31.2 MB (80.09%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        if(it==nums.end()) return -1;
+        else if(nums[it-nums.begin()]!=target) return -1;
+
+        return it-nums.begin();
+    }
+};
