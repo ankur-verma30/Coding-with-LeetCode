6a6a664de8b3428ab4c5d6e27e39d0ba0be82239
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2026-01-01 11:12:24 +0530

    Time: 0 ms (100%), Space: 43.5 MB (37.47%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n=digits.length, carry=1;
-        List <Integer> list=new ArrayList<>();
 
         for(int i=n-1;i>=0;i--){
             int sum=digits[i]+carry;
             if(sum>=10) {
                 sum=sum%10;
-                list.add(sum);
+                digits[i]=sum;
                 carry=1;
             }
             else {
-                list.add(sum);
+                digits[i]=(sum);
                 carry=0;
             }
 
         }
 
-        if(carry==1) list.add(1);
-        Collections.reverse(list);
-
-          int[] ans = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i);
+        if(carry==0) return digits;
+          int[] ans = new int[n+1];
+          ans[0]=1;
+        for (int i = 1; i <n+1; i++) {
+            ans[i] = digits[i-1];
         }
         return ans;
     }
