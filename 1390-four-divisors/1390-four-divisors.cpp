57146b0c8e3fc29d1b18232f71137c0c76077e8e
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2026-01-04 09:07:10 +0530

    Time: 31 ms (48.28%), Space: 16.9 MB (27.09%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,56 +1,34 @@
 class Solution {
 public:
-     inline int sum(int num){
-          int n =0,count =0;
-          for(int i =2;i<num;i++){
-              if(num%i==0){
-                  count++;
-                  n+=i;
-              }
-              if(count>=3){
-                  n =0;
-                  break;
-              }
-          }
-          if(count ==2){
-              return n +1+num;
-          }
-          return 0;
-      }
     int sumFourDivisors(vector<int>& nums) {
-         int ans =0;
-       for(int i=0;i<nums.size();i++){
-            ans += sum(nums[i]);
-       } 
-       return ans;
+        int totalSum = 0, n = nums.size();
+
+        unordered_map<int, int> haveFourDivisors;
+
+        for (int i = 0; i < n; i++) {
+            if (haveFourDivisors.count(nums[i])) {
+                totalSum += haveFourDivisors[nums[i]];
+                continue;
+            }
+            int count = 0, sum = 0, value = nums[i];
+            for (int j = 1; j * j <= value; j++) {
+                if (value % j == 0) {
+                    int quotient = value / j;
+                    if (quotient != j) {
+                        count += 2;
+                        sum += j + quotient;
+                    } else {
+                        count++;
+                        sum += j;
+                    }
+                }
+            }
+            if (count == 4) {
+                haveFourDivisors[value] = sum;
+                totalSum += sum;
+            }
+        }
+
+        return totalSum;
     }
 };
-
-        /*
-
-        This method has some issues
-         int ans =0;
-         int n =0,count =0,num=0,sum=0;
-
-       for(int i=0;i<nums.size();i++){
-           num=nums[i];
-           count=0;
-           n=0;
-           sum=0;
-          for(int i =2;i<num;i++){
-              if(num%i==0){
-                  count++;
-                  n+=i;
-              } 
-              if(count>=3){
-                  n =0;
-                  break;
-              }
-          if(count ==2)
-              sum=n+1+num; 
-          }
-          
-            ans += sum;
-       } 
-       return ans;
-    }  */
