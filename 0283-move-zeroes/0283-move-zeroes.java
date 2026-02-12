0b79e7f332e5ae483916e75f737bd09d0bdb2f3a
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2026-02-12 10:31:49 +0530

    Time: 2 ms (90.01%), Space: 47.9 MB (54.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,7 +4,6 @@ class Solution {
         int firstZeroIndex=0;
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0 && firstZeroIndex==-1) firstZeroIndex=i;
             if(nums[i]!=0){
                 int temp=nums[i];
                 nums[i]=nums[firstZeroIndex];
