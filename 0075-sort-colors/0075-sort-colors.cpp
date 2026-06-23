50c597122c70c5ac171cfa9307d7c147dc2ee527
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2026-06-24 00:35:25 +0530

    Time: 0 ms (100%), Space: 11.4 MB (97.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,35 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int len = nums.size();
-        if (len == 0)
-            return;
-
-        int low = 0, high = len - 1;
-        int temp;
-
-        while (low != high) {
-            while ((nums[low] == 0) && (low < high))
-                low++;
-            while ((nums[high] != 0) && (low < high))
-                high--;
-            if (low != high) {
-                temp = nums[low];
-                nums[low] = nums[high];
-                nums[high] = temp;
-            }
-        }
-        high = len - 1;
-        while (low != high) {
-            while ((nums[low] == 1) && (low < high))
-                low++;
-            while ((nums[high] != 1) && (low < high))
-                high--;
-            if (low != high) {
-                temp = nums[low];
-                nums[low] = nums[high];
-                nums[high] = temp;
-            }
-        }
+        sort(nums.begin(),nums.end());
     }
 };
