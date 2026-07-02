6d9ed386234f0fe7b1407b584e20df7a5cc1fefe
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2026-07-03 03:12:16 +0530

    Time: 0 ms (100%), Space: 15.9 MB (5.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
+    static bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.first == b.first) {
+            return a.second < b.second;
+        }
+        return a.first < b.first;
+    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
 
-        unordered_map<int, int> map;
+        int n = nums.size();
+        vector<pair<int, int>> sorted;
 
         for (int i = 0; i < n; i++) {
-            if (map.find(target - nums[i]) != map.end())
-                return {map[target - nums[i]], i};
-            map[nums[i]] = i;
+            sorted.push_back({nums[i], i});
+        }
+
+        sort(sorted.begin(), sorted.end(), cmp);
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int sum = sorted[left].first + sorted[right].first;
+
+            if (sum == target)
+                return {sorted[left].second, sorted[right].second};
+            else if (sum > target)
+                right--;
+            else
+                left++;
         }
 
         return {-1, -1};
