c413554e8242a94e0eca6073fc72c404a3ee7608
Ankur verma <109864349+ankur-verma30@users.noreply.github.com>
2026-07-18 09:00:58 +0530

    Time: 3 ms (68.06%), Space: 22.1 MB (57.83%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool faultyCheck(string& s, int faultyLeft, int faultyRight) {
+        int n = s.length();
+
+        // Delete faultyLeft
+        bool isLeftFaulty = false;
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            if (left == faultyLeft)
+                left++;
+            else if (s[left] != s[right]){
+                isLeftFaulty = true;
+                break;
+            }
+            else {
+                left++;
+                right--;
+            }
+        }
+
+        if (!isLeftFaulty)
+            return true;
+
+        // Delete faultyRight
+        bool isRightFaulty = false;
+        left = 0;
+        right = n - 1;
+
+        while (left < right) {
+            if (right == faultyRight)
+                right--;
+            else if (s[left] != s[right]){
+                isRightFaulty = true;
+                break;
+            }
+            else {
+                left++;
+                right--;
+            }
+        }
+
+        return !isRightFaulty;
+    }
+
+    bool validPalindrome(string s) {
+        int n = s.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (s[i] != s[n - 1 - i]) {
+                return faultyCheck(s, i, n - 1 - i);
+            }
+        }
+
+        return true;
+    }
+};
